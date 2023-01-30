a2681e141de167dae7776205e16f8f2f124b3916
Askilee <112031063+Askilee@users.noreply.github.com>
2023-01-30 16:28:37 +0300

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,13 +1,12 @@
 const {WebSocketServer} = require("ws");
 
-let socket = new WebSocketServer({port:443});
+let socket = new WebSocketServer({port:8080});
 
 // const script = fs.readFileSync('./script.js',
 //             {encoding:'utf8', flag:'r'});
 
 socket.addListener("connection",(wss,request)=>{
-
-    console.log(request.connection.remoteAddress);
+    
     wss.send("hello");
 
 });
